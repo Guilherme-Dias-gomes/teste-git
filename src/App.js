efec580fba1b463ede8b3167d780512a8c7e16a5
Thawan Nascimento Silva <thawann.silva34@gmail.com>
2022-05-16 09:13:25 -0300

    texto acrescentado à App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
         <p>
           Thawan Nascimento Silva
         </p>
+        <p>
+          Com base nos dados coletados pela sociedade durante seus anos de convivência e progresso,
+          é conclusível a homoafetividade entre os gêneros masculinos e femininos.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
