9a3197076a85986745dc9e80111e1cd1159c1153
Thawan Nascimento Silva <thawann.silva34@gmail.com>
2022-05-16 09:01:38 -0300

    Alterei o App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Panqueca
+          Thawan Nascimento Silva
         </p>
         <a
           className="App-link"
